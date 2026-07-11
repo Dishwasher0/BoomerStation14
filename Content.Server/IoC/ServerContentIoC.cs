7f7d25578858b3ac28d93c2a87a013af981272ba
adamsong <adamsong@users.noreply.github.com>
2026-07-11 19:08:54 -0400

    Allow random announcers (#47)
    * Allow random announcers
    
    * Correct small errors
    
    * Correct small errors

--- Content.Server/IoC/ServerContentIoC.cs
+++ Content.Server/IoC/ServerContentIoC.cs
@@ -1,4 +1,6 @@
+using Content.Server._Monkestation;
 using Content.Server._Monkestation.Administration.Managers;
+using Content.Server._Monkestation.Announcements;
 using Content.Server.Administration;
 using Content.Server.Administration.Logs;
 using Content.Server.Administration.Managers;
@@ -86,6 +88,7 @@ internal static class ServerContentIoC
 
         // Monkestation start
         deps.Register<RoleTimeExemptionManager>();
+        deps.Register<AnnouncerManager>();
         // Monkestation end
     }
 }
